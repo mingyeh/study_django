9383b7f11ea1fba6cccfcc89f124aaf0147c5d16
Ming Yeh <mingyeh1979@hotmail.com>
2015-12-07 21:43:15 +0800

    Add vote and result page to Poll

--- study_site/polls/views.py
+++ study_site/polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -17,8 +18,17 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    p = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/results.html', {'question':p})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    p = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = p.choice_set.get(pk = request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': p, 'error_message': "You didn't select a choice.",})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args = (p.id,)))
+
