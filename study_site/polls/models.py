651218f87a576c46c83ddb733801f72dcf57aef1
Ming Yeh <mingyeh1979@hotmail.com>
2015-12-13 20:38:32 +0800

    Add tests to Polls app.

--- study_site/polls/models.py
+++ study_site/polls/models.py
@@ -9,10 +9,11 @@ class Question(models.Model):
     pub_date = models.DateTimeField('Date published')
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+        now = timezone.now()
+        return now - datetime.timedelta(days = 1) <= self.pub_date <= now 
 	
-	def __str__(self):
-		return self.question_text
+    def __str__(self):
+	return self.question_text
     
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
--- study_site/polls/views.py
+++ study_site/polls/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext, loader
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -13,11 +14,16 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions. """
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte = timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Exclude any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte = timezone.now())
 
 class ResultView(generic.DetailView):
     model = Question
